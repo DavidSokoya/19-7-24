25c76e26b473494e3c8c1619d15447abd7dac916
davidsokoya <sokoya08@gmail.com>
2024-07-19 22:42:57 +0100

    Beginner Series #2 Clock

--- app.js
+++ app.js
@@ -11,3 +11,31 @@
 function numberToString(num) {
   return num.toSting();
 }
+
+// 2. Beginner Series #2 Clock
+// DESCRIPTION:
+// Clock shows h hours, m minutes and s seconds after midnight.
+
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+function past(h, m, s){
+  
+//  convert to seconds --> multiply the value of m by 60, and h by 360
+let timeInSeconds = ((h * 3600) + (m * 60) + s)
+
+// multiply the result by 1000 and return
+return timeInSeconds * 1000
+}
