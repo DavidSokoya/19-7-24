e42131fcec980197605b9d88b8d6db0bde6b609a
davidsokoya <sokoya08@gmail.com>
2024-07-19 22:43:59 +0100

    Rock Paper Scissors!

--- app.js
+++ app.js
@@ -39,3 +39,26 @@ let timeInSeconds = ((h * 3600) + (m * 60) + s)
 // multiply the result by 1000 and return
 return timeInSeconds * 1000
 }
+
+// Rock Paper Scissors!
+// DESCRIPTION:
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+
+  if(p1 === "scissors" && p2 === "paper" || p1 === "rock" && p2 === "scissors" || p1 === "paper" && p2 === "rock"  ){
+    return "Player 1 won!"
+  }
+  else  if(p1 === "scissors" && p2 === "rock" || p1 === "rock" && p2 === "paper" || p1 === "paper" && p2 === "scissors" ){
+    return "Player 2 won!"
+  }else  {
+    return "Draw!"
+  }
+};
