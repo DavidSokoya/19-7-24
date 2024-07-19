92f64f855537ffa54352acf84cb1a45f8c054011
davidsokoya <sokoya08@gmail.com>
2024-07-19 22:52:59 +0100

    Testing 1-2-3

--- app.js
+++ app.js
@@ -109,3 +109,34 @@ function powersOfTwo(n){
 
   return arr;
 }
+
+// 6. Testing 1-2-3
+
+// DESCRIPTION:
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples: (Input --> Output)
+
+// [] --> []
+// ["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+
+ function number(array){
+  //your awesome code here
+  let newArr = []
+ for (let i = 0; i < array.length; i++){
+   let res =  `${i + 1}: ${array[i]}`
+   newArr.push(res)
+ }
+return newArr
+}
+
+var number = function(array) {
+  return array.map(function (line, index) {
+    return (index + 1) + ": " + line;
+  });
+}
+
