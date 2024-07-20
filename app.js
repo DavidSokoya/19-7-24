9229d5b2bd50b9c63b26203d2a89c4406c1774ee
davidsokoya <sokoya08@gmail.com>
2024-07-20 22:50:58 +0100

    final

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // 1. Remove First and Last Character
-// DESCRIPTION:
-// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+
 
 function removeChar(str){
  //You got this!
@@ -27,4 +26,225 @@ return c
 
 function repeatStr (n, s) {
   return s.repeat(n);
+}
+
+// 3. Remove String Spaces
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+function noSpace(x){
+  let newStr = x.split(' ');
+  return newStr.join('')
+}
+
+function noSpace(x) {
+  return x.replaceAll(' ', '');
+}
+
+// 4. Convert a String to a Number!
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+const stringToNumber = function(str){
+  return Number(str);
+}
+
+// 5. Returning Strings
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// 6. Convert a Boolean to a String
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function booleanToString(b){
+  return b === true ? "true" : "false"
+}
+
+ function booleanToString(b){
+  return b.toString();
+}
+
+// 8. Are You Playing Banjo?
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+function areYouPlayingBanjo(name) {
+  let firstLetter = name.charAt(0).toLowerCase();
+
+  if(firstLetter ===  "r"){
+  return `${name} plays banjo`
+  }else {
+    return `${name} does not play banjo`;
+} }
+
+// 9. You only need one - Beginner
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+ return  a.includes(x) ? true : false
+}
+
+// 10. Sentence Smash
+
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+function smash (words) {
+   return words.join(' ')
+};
+
+// 11. DNA to RNA Conversion
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+// Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+// Create a function which translates a given DNA string into RNA.
+
+// For example:
+
+// "GCAT"  =>  "GCAU"
+// The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+
+function DNAtoRNA(dna) {
+  
+  return dna.replaceAll("T", "U")
+}
+
+// 12. If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+// 13. 
+
+var countSheep = function (num){
+  let murmur = '';
+  for (let i = 1; i <= num; i++){
+    murmur += `${i} sheep...`
+  }
+  return murmur;
+}
+
+// 14. Convert a string to an array
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+function stringToArray(string){
+    var result = [];
+    var traverse = function foo(string){
+        for(var i=0;i<string.length;i++){
+            var chr = string.charAt(i);
+            if(chr === " "){
+                result.push(string.slice(0,i));
+                foo(string.slice(i+1));
+                return;
+            }
+        }
+        result.push(string);
+    };
+    traverse(string);
+    return result;
+}
+
+// 12. Reversed Words
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+  let words = str.split(' ')
+ return words.reverse(' ').join(' ')
+}
+
+
+// 13. Total amount of points
+
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+function points(games) {
+    let ourScores = 0;
+  for (let i = 0; i < games.length; i++){
+    let ourTeam = games[i].charAt(0);
+    let otherTeam = games[i].charAt(2);
+    
+    if (ourTeam > otherTeam) {
+      ourScores += 3;
+    }else if (ourTeam < otherTeam){
+       ourScores += 0
+    }else{
+      ourScores += 1
+    }
+  }
+ return ourScores;
+}
+function points(games) {
+  let total = 0;
+  games.map(game => {
+    if (game[0] === game[2]) {
+      total += 1;
+    } else if (game[0] > game[2]) {
+      total += 3;
+    }
+  });
+  return total;
 }
